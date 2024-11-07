7996bb55e9eab043dc22e12c7a691d03290b5e50
Ana Edelinski <aedelinski85@gmail.com>
2024-11-07 13:54:06 +0100

    Implementation restart

--- target/classes/data-postgres.sql
+++ target/classes/data-postgres.sql
@@ -1,7 +1,3 @@
 insert into users (username, email, password, activated, name) values ('pera', 'pera@gmail.com', '$2a$10$phbu2oHcLQFL4PoXYyRW/erd.WJsHr9oAkuMxk0MoBdNjZ02Evxm2', true , 'pera')
 
-INSERT INTO post (description, image_url, like_count) VALUES ('Ovo je prvi post!', 'http://example.com/image1.jpg', 10);
-INSERT INTO post (description, image_url, like_count) VALUES ('Drugi post sa divnom slikom.', 'http://example.com/image2.jpg', 5);
-INSERT INTO post (description, image_url, like_count) VALUES ('Vreme je za učenje!', 'http://example.com/image3.jpg', 20);
-INSERT INTO post (description, image_url, like_count) VALUES ('Podeli svoje misli!', 'http://example.com/image4.jpg', 15);
-INSERT INTO post (description, image_url, like_count) VALUES ('Kako napreduješ sa projektom?', 'http://example.com/image5.jpg', 8);
+
