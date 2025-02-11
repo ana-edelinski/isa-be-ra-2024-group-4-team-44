4b1cedd14f0eead7a9245b17cf9bd98d12826014
Ana Edelinski <aedelinski85@gmail.com>
2025-02-11 18:01:18 +0100

    websocket

---
+++ websocket/src/main/java/rs/ac/uns/ftn/informatika/websockets/model/LocationMessage.java
@@ -0,0 +1,75 @@
+package rs.ac.uns.ftn.informatika.websockets.model;
+
+
+public class LocationMessage {
+
+    private Integer id;
+
+    private String name;
+
+    private String street;
+
+    private String city;
+
+    private String postalCode;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
